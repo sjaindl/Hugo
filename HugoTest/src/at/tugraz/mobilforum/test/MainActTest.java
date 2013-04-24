d08019a49ad60a31dc855526160aeb930f5c5a69
Andy <Andy@cp18-56.tu-graz.ac.at>
2013-04-24 10:41:47 +0200

    compiling version

--- HugoTest/src/at/tugraz/mobilforum/test/MainActTest.java
+++ HugoTest/src/at/tugraz/mobilforum/test/MainActTest.java
@@ -1,7 +1,20 @@
 package at.tugraz.mobilforum.test;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+import android.widget.LinearLayout;
+import at.tugraz.mobilforum.MainActivity;
 
-public class MainActTest extends ActivityInstrumentationTestCase2<T> {
+public class MainActTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
+	public MainActTest(Class<MainActivity> activityClass) {
+		super(activityClass);
+	}
+
+	@UiThreadTest
+	public void testCategoryView(){
+		LinearLayout categoryList = (LinearLayout)getActivity().findViewById(at.tugraz.mobilforum.R.id.categoryList);
+		assertNotNull(categoryList);
+	}
+	
 }
