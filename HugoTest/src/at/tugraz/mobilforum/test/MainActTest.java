ef0ffecb395320e9f877ed4d0b4d82cb771aefa7
Andy <Andy@cp18-56.tu-graz.ac.at>
2013-04-24 11:04:01 +0200

    another commit

--- HugoTest/src/at/tugraz/mobilforum/test/MainActTest.java
+++ HugoTest/src/at/tugraz/mobilforum/test/MainActTest.java
@@ -1,20 +1,33 @@
 package at.tugraz.mobilforum.test;
 
+import com.jayway.android.robotium.solo.Solo;
+
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.UiThreadTest;
 import android.widget.LinearLayout;
 import at.tugraz.mobilforum.MainActivity;
+import at.tugraz.mobilforum.R;
 
 public class MainActTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
+	private Solo solo;
+	
 	public MainActTest(Class<MainActivity> activityClass) {
 		super(activityClass);
 	}
 
+	@Override
+	public void setUp() throws Exception{
+		super.setUp();
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+	
 	@UiThreadTest
 	public void testCategoryView(){
-		LinearLayout categoryList = (LinearLayout)getActivity().findViewById(at.tugraz.mobilforum.R.id.categoryList);
-		assertNotNull(categoryList);
+		//LinearLayout categoryList = (LinearLayout)getActivity().findViewById(R.id.categoryList);
+		//assertNotNull(categoryList);
+		LinearLayout ll = (LinearLayout)solo.getView(R.id.categoryList);
+		assertNotNull(ll);
 	}
 	
 }
