044a66701c52bbe7f4a387537491408f72de93c8
Michael Spitzer <voxxxel@icloud.com>
2013-05-15 15:49:59 +0200

    issue #4 fix readentry, Michael Spitzer

---
+++ HugoTest/src/at/tugraz/mobilforum/test/ReadEntriesActivityTest.java
@@ -0,0 +1,76 @@
+package at.tugraz.mobilforum.test;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import org.junit.Test;
+
+import com.jayway.android.robotium.solo.Solo;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.AndroidTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.widget.EditText;
+import android.widget.ListView;
+import at.tugraz.mobilforum.AccessDataBase;
+import at.tugraz.mobilforum.Entry;
+import at.tugraz.mobilforum.R;
+import at.tugraz.mobilforum.ReadEntriesActivity;
+import at.tugraz.mobilforum.ReadEntriesBaseAdapter;
+
+
+public class ReadEntriesActivityTest extends ActivityInstrumentationTestCase2<ReadEntriesActivity>{
+
+
+	private Solo solo;
+	
+	public ReadEntriesActivityTest() {
+		super(ReadEntriesActivity.class);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected void setUp() throws Exception
+	{
+		AccessDataBase.setInstance(new AccessDataBase(this.getActivity().getApplicationContext()));
+		super.setUp();
+			solo = new Solo(getInstrumentation(), getActivity());
+	}
+	
+    
+	/** Testing Listview by checking its item count */
+	@SmallTest
+    public void testListViewEntryCounter(){
+    	ListView lv = (ListView)solo.getView(R.id.entryListView);
+    	int category_id = AccessDataBase.getInstance().getRandomCategory();
+    	int topic_id = AccessDataBase.getInstance().getRandomTopicFromCategory(category_id);
+    	int expectedCount = AccessDataBase.getInstance().getEntryList(topic_id).size();
+        int actualCount = lv.getChildCount();
+        assertEquals(expectedCount, actualCount);
+		assertEquals(true,true);
+    }
+	
+	/** Testing listview by getting a random entry and look for the entry in the database */
+	@SmallTest
+    public void testRandomListViewEntry(){
+    	ListView lv = (ListView)solo.getView(R.id.entryListView);
+    	//getEntryListCounter(int categoryid, int topicid)
+    	int category_id = AccessDataBase.getInstance().getRandomCategory();
+    	int topic_id = AccessDataBase.getInstance().getRandomTopicFromCategory(category_id);
+    	Entry random_entry = AccessDataBase.getInstance().getRandomEntryFromTopic(topic_id);
+    	String entrytext = random_entry.toString();
+    	boolean isentryinlist = false;
+    	for(int i=0;i<lv.getChildCount();i++){
+    	Entry entry = (Entry)lv.getItemAtPosition(i);
+    	if(entry.toString()==entrytext.toString()){
+    		isentryinlist = true;
+    		break;
+    		}
+    	}
+        assertEquals(isentryinlist, true);
+    }
+    
+}
---
+++ mobilForum/src/at/tugraz/mobilforum/ReadEntriesActivity.java
@@ -0,0 +1,65 @@
+package at.tugraz.mobilforum;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadEntriesActivity extends Activity {
+	
+	private ListView lv;
+	List<Entry> entries;
+	private int topicid = 1;
+	ReadEntriesBaseAdapter adapter;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		if(!AccessDataBase.hasInstance()){
+			AccessDataBase.setInstance(new AccessDataBase(this));
+		}
+		super.onCreate(savedInstanceState);
+		Bundle b = getIntent().getExtras();
+		this.topicid = b.getInt("topicId");
+		setContentView(R.layout.activity_read_forum);
+        lv = (ListView) findViewById(R.id.entryListView);
+        AccessDataBase db = AccessDataBase.getInstance();
+        /* TODO: gettopicid getcategory
+         * 
+         */
+        
+        entries = new ArrayList<Entry>();
+        entries = db.getEntryList(this.topicid);
+        
+        adapter = new ReadEntriesBaseAdapter(this,entries);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+					
+				 
+				// TODO Auto-generated method stub
+				
+			}
+        	 
+        	
+				
+			});
+ 
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.read_forum, menu);
+		
+		return true;
+	}
+
+}
